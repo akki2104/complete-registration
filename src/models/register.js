c200b830540b0023c6fb48fcec1b886b8ec78164
akki2104 <ay0832760@gmail.com>
2023-03-11 01:58:51 +0530

    create model

---
+++ src/models/register.js
@@ -0,0 +1,30 @@
+const { registerHelper } = require("hbs");
+const mongoose=require("mongoose");
+
+// schema
+const entries=new mongoose.Schema({
+    userName:{
+        type:String,
+        required:true,
+        min:3
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        min:6
+    },
+    conformPassword:{
+        type:String,
+        required:true,
+        min:6
+    }
+})
+
+// collection
+const Register=new mongoose.model("register",entries);
+module.exports=Register;
